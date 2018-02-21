4f1d3e9a58865c3659b7432fa13911959d4e1f47
WRupp <wagner.rupp@gmail.com>
2018-02-21 08:43:28 -0300

    Axis
    Co-Authored-By: wrupp <wrupp@users.noreply.github.com>

---
+++ FcsAxissimetrico/Caxis.m
@@ -0,0 +1,12 @@
+function Cr = Caxis(E,v)
+% Calcula a matriz de rigidez do material elastico linear isotropico
+% para o modelo axissimetrico
+
+cte = E / ((1+v)*(1-2*v));
+
+Cr = cte * [(1-v) v    v     0;
+             v   (1-v) v     0;
+             v    v    (1-v) 0;
+             0    0    0     (1-2*v)];      
+        
+end 

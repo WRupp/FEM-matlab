56c1424f4f6c73569113e21a218722dc8b5acf39
WRupp <WRupp@users.noreply.github.com>
2018-03-21 17:32:49 -0300

    Cubinho
    Um cubinho pro teste do Hex8

--- Benchmarks/Corpo de Prova/CCcubo.m
+++ Benchmarks/Corpo de Prova/CCcubo.m
@@ -1,19 +1,52 @@
 
-h = 1;  % altura do solido
+% h = 0;  % altura do solido
 
-f = 100 ; % forca por no - aplicada no nó
+f = 10000 ; % forca por no - aplicada no nó
 
+caminhoInput = 'C:\Users\Wagner\Desktop\Projeto FEM\FEM-matlab\Benchmarks\Corpo de Prova\';
+coordNome = 'CoordenadasNodais.txt';
+conecNome = 'Conectividade.txt';
 
-SetBaixo = NodePosYFinder(Ncoord,0);
-SetCima = NodePosYFinder(Ncoord,h); 
+lecoord = dlmread ([caminhoInput coordNome]);
+leconec = dlmread ([caminhoInput conecNome]);
 
-% SetMeio = NodePosFinder(Ncoord,posMeio);
+Ncoord(:,1) = lecoord(:,1);
+Ncoord(:,2:4) = lecoord(:,4:6);
+
+% Consertando a malha
+for i = 1 : size(Ncoord,1)
+    for j = 2 : 4
+    if Ncoord(i,j) < 1e-8
+      Ncoord(i,j) = 0 ;    
+    end    
+    end
+   if Ncoord(i,2) > 64.5
+      Ncoord(i,2) = 65 ;    
+    end
+end
+
+Ncoord = sortrows(Ncoord);
+
+Nconec(:,1:9) = leconec(:,11:end);
+
+% Construindo os sets por posicao
+
+SetEsq = NodePosFinder(Ncoord,0);
+SetDir = NodePosFinder(Ncoord,65);
+
+SetBaixoY = NodePosYFinder(Ncoord,0);
+SetBaixoZ = NodePosZFinder(Ncoord,0);
 
 % Criando o set de CC
+Mcc = [];
+Mcc = set2Mcc( SetEsq,0,1,Mcc);
+Mcc = set2Mcc( SetBaixoY,0,2,Mcc);
+Mcc = set2Mcc( SetBaixoZ,0,3,Mcc);
 
-Mcc = set2Mcc( SetBaixo,0,2,[]);
-Mcc = set2Mcc( SetBaixo,0,3,Mcc);
+Mcc = set2Mcc( SetDir,10,1,Mcc);
+
 
 % Criando o set de Forcas
 
-Mfn = set2Mcc( SetCima, f, 2,[]); 
+Mfn = [];
+% Mfn = set2Mcc( SetDir, f, 2,Mfn); 
